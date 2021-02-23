225fb44ecb20f26af47c93e39d1e084e3b376129
Mark Vlasenko <mvlasenko@deloitte.com>
2021-02-23 23:25:45 +0200

    fix

--- run_voice_control.py
+++ run_voice_control.py
@@ -7,17 +7,12 @@ import os
 import sys
 import model
 
-class ExecCommand(object):
-
-  def __init__(self):
-
-  def run_command(self, command, labels):
-    print("Executing: [{}]".format(command))
-    
-    if command == "exit_application":
-      sys.exit()
-    else:
-      os.system(command)
+def run_command(self, command):
+  print("Executing: [{}]".format(command))
+  if command == "exit_application":
+    sys.exit()
+  else:
+    os.system(command)
 
 def main():
   parser = argparse.ArgumentParser()
@@ -26,7 +21,6 @@ def main():
   interpreter = model.make_interpreter(args.model_file)
   interpreter.allocate_tensors()
   mic = args.mic if args.mic is None else int(args.mic)
-  exec_command = ExecCommand()
   sys.stdout.write("--------------------\n")
   sys.stdout.write("This script will execute shell commands via voice control.\n")
   sys.stdout.write("--------------------\n")
@@ -34,7 +28,7 @@ def main():
   model.classify_audio(mic, interpreter,
                        labels_file="config/labels_gc2.raw.txt",
                        commands_file="config/commands_exec.txt",
-                       dectection_callback=exec_command.run_command,
+                       dectection_callback=run_command,
                        sample_rate_hz=int(args.sample_rate_hz),
                        num_frames_hop=int(args.num_frames_hop))
 
